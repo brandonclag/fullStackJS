359adc582c7d90abd5e99b6ac704fee5d5f86f1a
Brandon <brclaggett2@gmail.com>
2020-07-05 21:34:45 -0500

    Added sass for styling the contests

--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -3,10 +3,10 @@ import React from 'react';
 // eslint-disable-next-line no-empty-pattern
 const ContestPreview = (contest) => (
   <div className='ContestPreview'>
-    <div>
+    <div className='category-name'>
       {contest.categoryName}
     </div>
-    <div>
+    <div className='contest-name'>
       {contest.contestName}
     </div>
   </div>
